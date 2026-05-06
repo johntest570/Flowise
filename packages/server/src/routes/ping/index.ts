87123e327bbc354ee4948bcbe246525454ab74bf
johntest570 <johntest570@yahoo.com>
2026-05-06 15:16:32 -0700

    fix(packages/server/src/routes/ping/index.ts): remediate MCP server must authenticate the client. [unifai-repo-scan]

--- packages/server/src/routes/ping/index.ts
+++ packages/server/src/routes/ping/index.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import pingController from '../../controllers/ping'
+import { authenticateUser } from '../../utils/validateKey'
 const router = express.Router()
 
 // GET
-router.get('/', pingController.getPing)
+router.get('/', authenticateUser, pingController.getPing)
 
 export default router
