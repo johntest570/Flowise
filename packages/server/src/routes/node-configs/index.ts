57055257d75c180cbce96befb6764a7942bd93f7
johntest570 <johntest570@yahoo.com>
2026-05-06 15:16:28 -0700

    fix(packages/server/src/routes/node-configs/index.ts): remediate MCP server must authenticate the client. [unifai-repo-scan]

--- packages/server/src/routes/node-configs/index.ts
+++ packages/server/src/routes/node-configs/index.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import nodeConfigsController from '../../controllers/node-configs'
+import { authenticateUser } from '../../utils/validateKey'
 const router = express.Router()
 
 // CREATE
-router.post('/', nodeConfigsController.getAllNodeConfigs)
+router.post('/', authenticateUser, nodeConfigsController.getAllNodeConfigs)
 
 export default router
